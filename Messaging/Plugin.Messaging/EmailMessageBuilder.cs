037ed2a836f94eababeb5cb04444f21451852d14
pingzing <neil.mcalister@futurice.com>
2016-11-04 17:17:10 +0200

    Make WithAttachment a little more PCL-friendly on Windows

--- Messaging/Plugin.Messaging/EmailMessageBuilder.cs
+++ Messaging/Plugin.Messaging/EmailMessageBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Plugin.Messaging
 {
@@ -57,8 +59,19 @@ namespace Plugin.Messaging
         public EmailMessageBuilder WithAttachment(string filePath, string contentType)
         {
 #if WINDOWS_PHONE_APP || WINDOWS_UWP
-            throw new PlatformNotSupportedException("API not supported on platform. Use EmailMessageBuilder.WithAttachment(Windows.Storage.IStorageFile file) overload instead");
-
+            try
+            {
+                var file = Task.Factory.StartNew(() => Windows.Storage.StorageFile.GetFileFromPathAsync(filePath).AsTask())
+                    .Unwrap()
+                    .GetAwaiter()
+                    .GetResult();
+                _email.Attachments.Add(new EmailAttachment(file));
+                return this;
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                throw new PlatformNotSupportedException("Windows apps cannot access files by filePath unless they reside in ApplicationData. Use the platform-specific WithAttachment(IStorageFile) overload instead.");
+            }
 #elif __ANDROID__ || __IOS__
             _email.Attachments.Add(new EmailAttachment(filePath, contentType));
             return this;
